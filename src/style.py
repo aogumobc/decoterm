a5fa1dbfa29929cc3c4f0298223679df5e860bba
aogumobc <aogumobc@gmail.com>
2023-03-04 15:03:21 +0900

    added: style class

---
+++ src/style.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class Font_Style(Enum):
+    BOLD           = 1
+    FAINT          = 2
+    ITALIC         = 3
+    UNDERLINE      = 4
+    BLINK          = 5
+    RAPIDBLINK     = 6
+    REVERSE        = 7
+    CONCEAL        = 8
+    CROSSEDOUT     = 9
+    DEFAULT_FONT   = 10
+    FRAKTUR        = 20
+    BOLD_OFF       = 21
+    FAINT_OFF      = 22
+    ITALIC_OFF     = 23
+    UNDERLINE_OFF  = 24
+    BLINK_OFF      = 25
+    PROPRIT_OFF    = 26
+    REVERSE_OFF    = 27
+    CONCEAL_OFF    = 28
+    CROSSEDOUT_OFF = 29
+    PROPRIT        = 50
